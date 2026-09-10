6f855ba9bc7c6206c2501a9b3fdc6ef1c7bceb32
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-09-10 13:29:08 +0500

    Add v0.11 realtime 3D boot sequence

---
+++ ColorizeDesignWeb/boot-v011.js
@@ -0,0 +1,7 @@
+import './v05-frame-sync.js';
+import './v06-font-sync.js';
+import './v011-realtime-3d.js';
+import './app.js';
+import './v081-ui-fix.js';
+import './facade.js';
+import './v010-workspace-safe.js';
