c204c81de406aa63a71d0b2c9e5c5a2134fa0491
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-09-11 17:56:34 +0500

    Colorize Design v0.19.3: refresh renderer cache keys

--- ColorizeDesignWeb/boot-v019.js
+++ ColorizeDesignWeb/boot-v019.js
@@ -1,7 +1,7 @@
-import './boot-v018.js?v=0192';
+import './boot-v018.js?v=0193';
 
 const search=location.search||'';
 const legacySmoke=search.includes('smoke')&&!search.includes('v019-smoke');
 if(!legacySmoke){
-  import('./v019-separate-parts.js?v=0192').then(()=>import('./v019-parts-ui.js?v=0192'));
+  import('./v019-separate-parts.js?v=0193').then(()=>import('./v019-parts-ui.js?v=0193'));
 }
