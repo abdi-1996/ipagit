4b0ed2530dcb7903a51de7ef8c6ec9381934906a
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-09-11 17:16:47 +0500

    Colorize Design v0.19.2: load live parts UI after renderer

--- ColorizeDesignWeb/boot-v019.js
+++ ColorizeDesignWeb/boot-v019.js
@@ -1,5 +1,7 @@
-import './boot-v018.js?v=0191';
+import './boot-v018.js?v=0192';
 
 const search=location.search||'';
 const legacySmoke=search.includes('smoke')&&!search.includes('v019-smoke');
-if(!legacySmoke) import('./v019-separate-parts.js?v=0191');
+if(!legacySmoke){
+  import('./v019-separate-parts.js?v=0192').then(()=>import('./v019-parts-ui.js?v=0192'));
+}
