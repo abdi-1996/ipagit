be330eb978fd06704dc8963454068357a4b72381
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-09-12 02:03:22 +0500

    Colorize Design v0.27.1: keep HDRI passive in normal light mode

--- ColorizeDesignWeb/boot-v027.js
+++ ColorizeDesignWeb/boot-v027.js
@@ -1,2 +1,3 @@
-import './boot-v0251.js?v=0270';
-import './v027-unified-hdri.js?v=0270';
+import './boot-v0251.js?v=0271';
+import './v027-unified-hdri.js?v=0271';
+import './v0271-hdri-passive-mode.js?v=0271';
