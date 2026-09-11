3d815c005f88c68b5a173be0f8a15647f934325f
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-09-11 17:10:17 +0500

    Colorize Design v0.19.1: isolate new renderer from legacy smoke tests

--- ColorizeDesignWeb/boot-v019.js
+++ ColorizeDesignWeb/boot-v019.js
@@ -1,2 +1,5 @@
-import './boot-v018.js?v=0190';
-import './v019-separate-parts.js?v=0190';
+import './boot-v018.js?v=0191';
+
+const search=location.search||'';
+const legacySmoke=search.includes('smoke')&&!search.includes('v019-smoke');
+if(!legacySmoke) import('./v019-separate-parts.js?v=0191');
